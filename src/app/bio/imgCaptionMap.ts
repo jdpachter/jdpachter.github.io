748be26c069f70a3311c5f923e720128ba86100d
jdpachter <jdpachter@gmail.com>
2021-02-28 12:40:48 -0800

    Match caption width to image and parent

--- src/app/bio/imgCaptionMap.ts
+++ src/app/bio/imgCaptionMap.ts
@@ -4,7 +4,7 @@ export let BIOIMAGES = new Map([
   ['sf', new BioImageData('assets/bioImages/sf.jpg', 'Downtown SF at dusk as seen from Kite Hill in Eureka Valley')],
   ['amzn', new BioImageData('assets/bioImages/amznHQ.jpg', 'The Amazon Spheres at the South Lake Union Headquarters in Seattle')],
   ['lex', new BioImageData('assets/bioImages/lex.jpg', 'The annual battle of Lexington re-enactment in Lexington, MA')],
-  ['phl', new BioImageData('assets/bioImages/schoolofathens.jpg', 'Raphael\'s famous School of Athens painting depicting Plato and Aristotle. Plato points towards the sky in a nod to Platonian Idealism while Aristotle gestures towards the ground to remind us of his belief in an empirical reality.')],
+  ['phl', new BioImageData('assets/bioImages/schoolofathens.jpg', 'Raphael\'s School of Athens depicting Plato and Aristotle. Plato points towards the sky in a nod to Platonian Idealism while Aristotle gestures towards the ground to remind us of his belief in an empirical reality.')],
   ['studying', new BioImageData('assets/bioImages/studying.png', '"Studying" in the library')],
   ['cs', new BioImageData('assets/bioImages/cs.jpg', 'This tweet hits hard')],
   ['ur', new BioImageData('assets/bioImages/ur.jpg', 'Rush Rhees, UR\'s main library')],
